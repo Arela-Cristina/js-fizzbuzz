565f182585dfc98902fbe476062092fd1048d0a0
Arela-Cristina <instagrandine@gmail.com>
2024-10-15 15:04:59 +0200

    init

---
+++ main.js
@@ -0,0 +1,12 @@
+//FIZZ BUZZ
+
+//Scrivi un programma che stampi i numeri da 1 a 100
+//Per i multipli di 3 che di 5 stampi "FizzBuzz"
+//Per i multipli di 3 stampi “Fizz” al posto del numero 
+//Per i multipli di 5 stampi "Buzz" al posto del numero.
+
+//Consigli:
+//Prima di partire a scrivere codice poniamoci qualche domanda:
+//Come faccio a sapere se un numero è divisibile per?
+//Abbiamo visto qualcosa di particolare che possiamo usare?
+
