6cfcd0789847bdea057a22f4d14a49cdde12fb72
Arela-Cristina <instagrandine@gmail.com>
2024-10-15 15:34:12 +0200

    Third condition

--- main.js
+++ main.js
@@ -10,17 +10,15 @@ for (let i = 0; i < 100; i++) {
     let num = i + 1;
     // console.log(num);
 
-
-if (num % 3 === 0 && num % 5 === 0) { 
-    console.log("FizzBuzz");
-    //Per i multipli sia di 3 che di 5 stampi "FizzBuzz"
-}else if (num % 3 === 0) { 
-    console.log("Fizz");
-     //Per i multipli di 3 stampi “Fizz” al posto del numero
-} 
-
-
-//Per i multipli di 5 stampi "Buzz" al posto del numero.
-
+    if (num % 3 === 0 && num % 5 === 0) {
+        console.log("FizzBuzz");
+        //Per i multipli sia di 3 che di 5 stampi "FizzBuzz"
+    } else if (num % 3 === 0) {
+        console.log("Fizz");
+        //Per i multipli di 3 stampi “Fizz” al posto del numero
+    } else if (num % 5 === 0) {
+        console.log("Buzz");
+        //Per i multipli di 5 stampi "Buzz" al posto del numero.
+    }
 
 }
